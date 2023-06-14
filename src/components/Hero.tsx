4b1a68e34f4bd0e0aa6b676422d795572d2354e7
Doyeon Kim <s21250535@sjajeju.kr>
2023-06-14 11:49:56 +0900

    hero picture

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='font-andika'>HI, this is TYJ Website</div>
+    <div className="relative py-32 w-full h-full">
+      <div className="absolute top-0 justify items-center mx-auto justify-center object-none w-screen h-full">
+        <span className="w-screen h-full absolute opacity-75 bg-black"></span>
+        <img src="https://i1.wp.com/www.korvia.com/wp-content/uploads/2012/07/image-facebook-jeju-island-beach-guide-cover.jpg?fit=1200%2C628&ssl=1" alt="image" className="mx-auto w-screen h-full" />
+      </div>
+      <div className="relative w-screen lg:px-32 md:px-20 sm:px-8">
+        <div className="flex flex-wrap w-full mx-auto text-center justify-center">
+          <h1 className="text-white font-shadows text-5xl">
+            Dear Jeju, “Thank You!” 
+          </h1>
+          <p className="mt-4 text-lg text-gray-300 font-andika">
+            Welcome to our community service website! We are a group of individuals dedicated to making a positive impact in our community through various service projects and events. Whether you are an individual looking to get involved and give back, or an organization seeking volunteers for your cause, we hope to connect you with opportunities to make a difference.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
