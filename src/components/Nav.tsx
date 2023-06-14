0896f09f03553c9efcf0f4b50e057bc532179e72
Doyeon Kim <s21250535@sjajeju.kr>
2023-06-14 12:02:55 +0900

    nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,33 +3,84 @@ import React from 'react'
 const Nav = () => {
   return (
     <div>
-      <nav className=" w-screen flex flex-wrap items-center justify-around py-8 px-10 bg-slate-200">
-        <div className="">
-          (logo)
+      <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 ">
+            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                    <a
+                        className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                        href="#">THANK YOU JEJU</a>
+                    <button
+                        className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                        type="button"
+                    >
+                        <i className="text-white fas fa-bars"></i>
+                    </button>
+                </div>
+            <div
+            className="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none hidden"
+            id="example-collapse-navbar">
+                <ul className="flex flex-col lg:flex-row  lg:px-6 list-none lg:ml-auto">
+                    <li className="flex items-center">
+                        <a
+                            className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                            href="https://github.com/oculit-dev">
+                            
+                            <a
+                                className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-800 lg:text-gray-300"
+                                href="#">About Us</a>
+                        </a>
+                    </li>
+                    <li className="flex items-center">
+                        <a
+                            className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                            href="https://github.com/oculit-dev">
+                            
+                            <a
+                                className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-800 lg:text-gray-300"
+                                href="#">Gallery</a>
+                        </a>
+                    </li>
+                    <li className="flex items-center">
+                        <a
+                            className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                            href="https://github.com/oculit-dev">
+                            
+                            <a
+                                className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-800 lg:text-gray-300"
+                                href="#">Shop</a>
+                        </a>
+                    </li>
+                    <li className="flex items-center">
+                        <a
+                            className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                            href="https://www.instagram.com/thankyoujeju_gec/">
+
+                            <i className="lg:text-gray-300 text-gray-500 fab fa-instagram text-lg leading-lg "></i>
+                            <span className="lg:hidden inline-block ml-2">Instagram</span>
+                        </a>
+                    </li>
+                    <li className="flex items-center">
+                        <a
+                            className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                            href="https://github.com/oculit-dev">
+
+                            <i className="lg:text-gray-300 text-gray-500 fab fa-github text-lg leading-lg "></i>
+                            <span className="lg:hidden inline-block ml-2">Star</span>
+                        </a>
+                    </li>
+                    <li className="flex items-center justify-between">
+                        <li className="flex items-center justify-between">
+                            <div className="inline-flex w-full sm:w-auto">
+                                <a href="#" className="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3">
+                                    <i className=""></i> Register Now
+                                </a>
+                            </div>
+                        </li>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div className='justify-between flex'>
-          <div className='mx-10 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-600 hover:underline'>
-            About Us
-          </div>
-          <div className="mx-10 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-600 hover:underline">
-            Environmental Issues
-          </div>
-          <div className="mx-10 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-600 hover:underline">
-            Gallery
-          </div>
-        </div>
-        <div className="justify-between flex">
-          <div className='px-2'>
-            Login
-          </div>
-          <div>
-            |
-          </div>
-          <div className='px-2'>
-            SignUp
-          </div>
-        </div>
-      </nav>
+    </nav>
     </div>
   )
 }
