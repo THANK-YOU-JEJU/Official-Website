32077f61ae7bbc379756271245269c2177ae2316
Doyeon Kim <s21250535@sjajeju.kr>
2023-06-14 10:53:42 +0900

    new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+
 function App() {
   return (
-    <div className="">
-      Thank You Jeju
+    <div className="container">
+      <Nav />
+      <Hero />
     </div>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='font-andika'>HI, this is TYJ Website</div>
   )
 }
 
