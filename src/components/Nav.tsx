e78c0e6378a92b199ff95fca53b9adda8ea523e0
SadPig0 <119029502+SadPig0@users.noreply.github.com>
2023-06-14 11:57:01 +0900

    asdf

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+      <nav className=" w-screen flex flex-wrap items-center justify-around py-8 px-10 bg-slate-200">
+        <div className="">
+          (logo)
+        </div>
+        <div className='justify-between flex'>
+          <div className='mx-10 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-600 hover:underline'>
+            About Us
+          </div>
+          <div className="mx-10 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-600 hover:underline">
+            Environmental Issues
+          </div>
+          <div className="mx-10 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-purple-400 hover:to-pink-600 hover:underline">
+            Gallery
+          </div>
+        </div>
+        <div className="justify-between flex">
+          <div className='px-2'>
+            Login
+          </div>
+          <div>
+            |
+          </div>
+          <div className='px-2'>
+            SignUp
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 }
 
