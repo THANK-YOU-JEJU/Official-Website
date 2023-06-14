fe80cfb315c733384d1f68e00db08acdc85ccdb1
SadPig0 <119029502+SadPig0@users.noreply.github.com>
2023-06-14 12:41:20 +0900

    hello
    pl

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Hero.css'
+
 
 const Hero = () => {
   return (
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,87 +1,64 @@
 import React from 'react'
-
+import {AiFillGithub} from "react-icons/ai";
+import {AiOutlineInstagram} from "react-icons/ai"
 const Nav = () => {
   return (
-    <div>
-      <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 ">
-            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-                <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-                    <a
-                        className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-                        href="#">THANK YOU JEJU</a>
-                    <button
-                        className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                        type="button"
-                    >
-                        <i className="text-white fas fa-bars"></i>
-                    </button>
-                </div>
+    <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 ">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+            <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                <a
+                    className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                    href="#">THANK YOU JEJU</a>
+                <button
+                    className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                    type="button"
+                >
+                    <i className="text-white fa-bars"></i>
+                </button>
+            </div>
             <div
             className="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none hidden"
             id="example-collapse-navbar">
                 <ul className="flex flex-col lg:flex-row  lg:px-6 list-none lg:ml-auto">
                     <li className="flex items-center">
                         <a
-                            className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                            href="https://github.com/oculit-dev">
-                            
-                            <a
-                                className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-800 lg:text-gray-300"
-                                href="#">About Us</a>
-                        </a>
+                            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-800 lg:text-gray-300"
+                            href="#">About Us</a>
                     </li>
                     <li className="flex items-center">
-                        <a
-                            className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                            href="https://github.com/oculit-dev">
-                            
                             <a
                                 className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-800 lg:text-gray-300"
                                 href="#">Gallery</a>
-                        </a>
+                    </li>
+                    <li className="flex items-center">
+                        <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-800 lg:text-gray-300" href="#">Shop</a>
                     </li>
                     <li className="flex items-center">
                         <a
-                            className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                            href="https://github.com/oculit-dev">
-                            
-                            <a
-                                className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-800 lg:text-gray-300"
-                                href="#">Shop</a>
-                        </a>
-                    </li>
-                    <li className="flex items-center">
-                        <a
-                            className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                            className="lg:text-white lg:hover:text-gray-300 px-3 py-4 lg:py-2 flex items-center"
                             href="https://www.instagram.com/thankyoujeju_gec/">
-
-                            <i className="lg:text-gray-300 text-gray-500 fab fa-instagram text-lg leading-lg "></i>
-                            <span className="lg:hidden inline-block ml-2">Instagram</span>
+                            <AiOutlineInstagram />
                         </a>
                     </li>
-                    <li className="flex items-center">
+                    <li className="flex items-center ">
                         <a
                             className="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
                             href="https://github.com/oculit-dev">
-
-                            <i className="lg:text-gray-300 text-gray-500 fab fa-github text-lg leading-lg "></i>
-                            <span className="lg:hidden inline-block ml-2">Star</span>
+                            <AiFillGithub />
                         </a>
                     </li>
                     <li className="flex items-center justify-between">
-                        <li className="flex items-center justify-between">
-                            <div className="inline-flex w-full sm:w-auto">
-                                <a href="#" className="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3">
-                                    <i className=""></i> Register Now
-                                </a>
-                            </div>
-                        </li>
+                        <div className="inline-flex w-full sm:w-auto">
+                            <a href="#" className="bg-white text-gray-800 active:bg-gray-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3">
+                                <i className=""></i> Register Now
+                            </a>
+                        </div>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-    </div>
+    
   )
 }
 
