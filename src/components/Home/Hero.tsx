a7054bc41cb2072e8a57d75c4155fd8900ddbd53
Doyeon Kim <s21250535@sjajeju.kr>
2023-06-26 19:10:06 +0900

    latest commit

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import '../../App.css'
 
+import {
+  Typography,
+  Card,
+} from "@material-tailwind/react";
 
 const Hero = () => {
   return (
     <div className="">
-      {/* JOING US NOW */}
+      <div className="mx-auto max-w-screen-md py-12">
+         <Card className="mb-12 overflow-hidden rounded-xl">
+           <img
+             alt="nature"
+             className="h-[32rem] w-full object-cover object-center"
+             src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
+           />
+         </Card>
+         <Typography variant="h2" color="blue-gray" className="mb-2">
+           Dear Jeju, “Thank You!” 
+         </Typography>
+         <Typography color="gray" className="font-normal">
+           Welcome to our community service website! We are a group of individuals dedicated 
+           to making a positive impact in our community through various service projects and events. 
+           Whether you are an individual looking to get involved and give back, or an organization 
+           seeking volunteers for your cause, we hope to connect you with opportunities to make a 
+           difference. We collaborate with BHA and SJA to help protect the environment on Jeju Island. 
+           Together, we address environmental issues such as pollution by combining resources. 
+           Whether through cleanups, tree planting, or educational campaigns, we work to raise 
+           awareness and drive sustainable change in the community. Our community service group 
+           stands out by collaborating with other organizations, prioritizing education and 
+           awareness, and using an online store to raise funds. We are a group of individuals 
+           dedicated to making a positive impact in our community through various service projects 
+           and events. 
+         </Typography>
+       </div>
+      {/* JOIN US NOW */}
       <section className="w-screen lg:py-12 lg:flex lg:justify-center">
         <div className="overflow-hidden bg-gray-900 lg:mx-8 lg:flex lg:max-w-6xl lg:w-full lg:shadow-md lg:rounded-xl">
           <div className="lg:w-1/2">
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,214 @@
 import React from "react";
 import {
   Navbar,
-  MobileNav,
+  Collapse,
   Typography,
   Button,
   IconButton,
-  Card,
+  List,
+  ListItem,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Chip,
 } from "@material-tailwind/react";
+import {
+  ChevronDownIcon,
+  UserCircleIcon,
+  CubeTransparentIcon,
+  Bars3Icon,
+  XMarkIcon,
+  FlagIcon,
+  ChatBubbleOvalLeftIcon,
+  UsersIcon,
+  FolderIcon,
+  Square3Stack3DIcon,
+  RocketLaunchIcon,
+  FaceSmileIcon,
+  PuzzlePieceIcon,
+  GiftIcon,
+} from "@heroicons/react/24/outline";
+ 
+const colors = {
+  blue: "bg-blue-50 text-blue-500",
+  orange: "bg-orange-50 text-orange-500",
+  green: "bg-green-50 text-green-500",
+  "blue-gray": "bg-blue-gray-50 text-blue-gray-500",
+  purple: "bg-purple-50 text-purple-500",
+  teal: "bg-teal-50 text-teal-500",
+  cyan: "bg-cyan-50 text-cyan-500",
+  pink: "bg-pink-50 text-pink-500",
+};
+ 
+const navListMenuItems = [
+  {
+    color: "blue",
+    icon: FlagIcon,
+    title: "About us",
+    description: "Learn about our story and our mission statement.",
+  },
+  {
+    color: "orange",
+    icon: ChatBubbleOvalLeftIcon,
+    title: "Press",
+    description: "News and writings, press releases, and resources",
+  },
+  {
+    color: "green",
+    icon: UsersIcon,
+    title: (
+      <div className="flex items-center gap-1">
+        Careers{" "}
+        <Chip
+          size="sm"
+          color="green"
+          variant="ghost"
+          value="We're hiring!"
+          className="capitalize"
+        />
+      </div>
+    ),
+    description: "We are always looking for talented people. Join us!",
+  },
+  {
+    color: "blue-gray",
+    icon: FolderIcon,
+    title: "Legal",
+    description: "All the stuff that we dan from legal made us add.",
+  },
+  {
+    color: "purple",
+    icon: RocketLaunchIcon,
+    title: "Products",
+    description: "Checkout our products that helps a startup running.",
+  },
+  {
+    color: "teal",
+    icon: FaceSmileIcon,
+    title: "Icons",
+    description: "Set of beautiful icons that you can use in your project.",
+  },
+  {
+    color: "cyan",
+    icon: PuzzlePieceIcon,
+    title: "UI Kits",
+    description: "High quality UI Kits helps you to 2x faster.",
+  },
+  {
+    color: "pink",
+    icon: GiftIcon,
+    title: "Open Source",
+    description: "List of all our open-source projects, it's all free.",
+  },
+];
+ 
+function NavListMenu() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
+ 
+  const renderItems = navListMenuItems.map(
+    ({ icon, title, description, color }, key) => (
+      <a href="#" key={key}>
+        <MenuItem className="flex items-center gap-3 rounded-lg">
+          <div className={`rounded-lg p-5 ${color}`}>
+            {React.createElement(icon, {
+              strokeWidth: 2,
+              className: "h-6 w-6",
+            })}
+          </div>
+          <div>
+            <Typography
+              variant="h6"
+              color="blue-gray"
+              className="flex items-center text-sm"
+            >
+              {title}
+            </Typography>
+            <Typography variant="small" color="gray" className="font-normal">
+              {description}
+            </Typography>
+          </div>
+        </MenuItem>
+      </a>
+    )
+  );
+ 
+  return (
+    <React.Fragment>
+      <Menu
+        open={isMenuOpen}
+        handler={setIsMenuOpen}
+        offset={{ mainAxis: 20 }}
+        placement="bottom"
+        allowHover={true}
+      >
+        <MenuHandler>
+          <Typography as="div" variant="small" className="font-normal  text-gray-900">
+            <ListItem
+              className="flex items-center gap-2 py-2 pr-4"
+              selected={isMenuOpen || isMobileMenuOpen}
+              onClick={() => setIsMobileMenuOpen((cur) => !cur)}
+            >
+              <Square3Stack3DIcon className="h-[18px] w-[18px]" />
+              Resources
+              <ChevronDownIcon
+                strokeWidth={2.5}
+                className={`hidden h-3 w-3 transition-transform lg:block ${
+                  isMenuOpen ? "rotate-180" : ""
+                }`}
+              /> 
+              <ChevronDownIcon
+                strokeWidth={2.5}
+                className={`block h-3 w-3 transition-transform lg:hidden ${
+                  isMobileMenuOpen ? "rotate-180" : ""
+                }`}
+              />
+            </ListItem>
+          </Typography>
+        </MenuHandler>
+        <MenuList className="hidden max-w-screen-xl rounded-xl lg:block">
+          <ul className="grid grid-cols-4 gap-y-2">{renderItems}</ul>
+        </MenuList>
+      </Menu>
+      <div className="block lg:hidden">
+        <Collapse open={isMobileMenuOpen}>{renderItems}</Collapse>
+      </div>
+    </React.Fragment>
+  );
+}
+ 
+function NavList() {
+  return (
+    <List className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1">
+      <Typography
+        as="a"
+        href="#"
+        variant="small"
+        color="blue-gray"
+        className="font-normal"
+      >
+        <ListItem className="flex items-center gap-2 py-2 pr-4 text-gray-900">
+          <CubeTransparentIcon className="h-[18px] w-[18px]" />
+          Our Story
+        </ListItem>
+      </Typography>
+      <NavListMenu />
+      <Typography
+        as="a"
+        href="#"
+        variant="small"
+        color="blue-gray"
+        className="font-normal"
+      >
+        <ListItem className="flex items-center gap-2 py-2 pr-4  text-gray-900">
+          <UserCircleIcon className="h-[18px] w-[18px]" />
+          Account
+        </ListItem>
+      </Typography>
+    </List>
+  );
+}
  
 export default function Example() {
   const [openNav, setOpenNav] = React.useState(false);
@@ -18,133 +220,198 @@ export default function Example() {
     );
   }, []);
  
-  const navList = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="#" className="flex items-center text-gray-900">
-          Our Story
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="#" className="flex items-center text-gray-900">
-          Environmental Issues
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-normal"
-      >
-        <a href="#" className="flex items-center text-gray-900">
-          Gallery
-        </a>
-      </Typography>
-    </ul>
-  );
- 
   return (
-    <>
-      <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
-        <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography
-            as="a"
-            href="#"
-            className="mr-4 cursor-pointer py-1.5 text-gray-900"
-          >
-            THANK YOU JEJU
-          </Typography>
-          <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
-            <Button
-              variant="gradient"
-              size="sm"
-              className="hidden lg:inline-block"
-            >
-              <span>Buy Now</span>
-            </Button>
-            <IconButton
-              variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
-            >
-              {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </IconButton>
-          </div>
+    <Navbar className="sticky inset-0 z-10 h-max w-full rounded-lg py-2 px-4 my-4 bg-white/30 shadow-xl backdrop-blur-3xl lg:px-8 lg:py-4 mx-auto max-w-screen-xl">
+      <div className="flex items-center justify-between text-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          variant="h6"
+          className="mr-4 cursor-pointer py-1.5 lg:ml-2"
+        >
+          THANK YOU JEJU
+        </Typography>
+        <div className="hidden lg:block">
+          <NavList />
         </div>
-        <MobileNav open={openNav}>
-          {navList}
-          <Button size="sm" fullWidth className="mb-2 bg-teal-800">
-            <span>Buy Now</span>
+        <div className="hidden gap-2 lg:flex">
+          <Button className="bg-gray-900 text-sm p-2">
+            Register Now
           </Button>
-        </MobileNav>
-      </Navbar>
-      <div className="mx-auto max-w-screen-md py-12">
-        <Card className="mb-12 overflow-hidden rounded-xl">
-          <img
-            alt="nature"
-            className="h-[32rem] w-full object-cover object-center"
-            src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
-          />
-        </Card>
-        <Typography variant="h2" color="blue-gray" className="mb-2">
-          Dear Jeju, “Thank You!” 
-        </Typography>
-        <Typography color="gray" className="font-normal">
-          Welcome to our community service website! We are a group of individuals dedicated 
-          to making a positive impact in our community through various service projects and events. 
-          Whether you are an individual looking to get involved and give back, or an organization 
-          seeking volunteers for your cause, we hope to connect you with opportunities to make a 
-          difference. We collaborate with BHA and SJA to help protect the environment on Jeju Island. 
-          Together, we address environmental issues such as pollution by combining resources. 
-          Whether through cleanups, tree planting, or educational campaigns, we work to raise 
-          awareness and drive sustainable change in the community. Our community service group 
-          stands out by collaborating with other organizations, prioritizing education and 
-          awareness, and using an online store to raise funds. We are a group of individuals 
-          dedicated to making a positive impact in our community through various service projects 
-          and events. 
-        </Typography>
+        </div>
+        <IconButton
+          variant="text"
+          color="blue-gray"
+          className="lg:hidden"
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+          ) : (
+            <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+          )}
+        </IconButton>
       </div>
-    </>
+      <Collapse open={openNav}>
+        <NavList />
+        <div className="flex w-full flex-nowrap items-center gap-2 lg:hidden bg-gray-900 text-gray-900">
+          <Button>
+            Register Now
+          </Button>
+        </div>
+      </Collapse>
+    </Navbar>
   );
 }
+
+// import React from "react";
+// import {
+//   Navbar,
+//   MobileNav,
+//   Typography,
+//   Button,
+//   IconButton,
+//   Card,
+// } from "@material-tailwind/react";
+ 
+// export default function Example() {
+//   const [openNav, setOpenNav] = React.useState(false);
+ 
+//   React.useEffect(() => {
+//     window.addEventListener(
+//       "resize",
+//       () => window.innerWidth >= 960 && setOpenNav(false)
+//     );
+//   }, []);
+ 
+//   const navList = (
+//     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+//       <Typography
+//         as="li"
+//         variant="small"
+//         color="blue-gray"
+//         className="p-1 font-normal"
+//       >
+//         <a href="#" className="flex items-center text-gray-900">
+//           Our Story
+//         </a>
+//       </Typography>
+//       <Typography
+//         as="li"
+//         variant="small"
+//         color="blue-gray"
+//         className="p-1 font-normal"
+//       >
+//         <a href="#" className="flex items-center text-gray-900">
+//           Environmental Issues
+//         </a>
+//       </Typography>
+//       <Typography
+//         as="li"
+//         variant="small"
+//         color="blue-gray"
+//         className="p-1 font-normal"
+//       >
+//         <a href="#" className="flex items-center text-gray-900">
+//           Gallery
+//         </a>
+//       </Typography>
+//     </ul>
+//   );
+ 
+//   return (
+//     <>
+//       <Navbar className="sticky inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 bg-white opacity-90 lg:px-8 lg:py-4">
+//         <div className="flex items-center justify-between text-blue-gray-900">
+//           <Typography
+//             as="a"
+//             href="#"
+//             className="mr-4 cursor-pointer py-1.5 text-gray-900"
+//           >
+//             THANK YOU JEJU
+//           </Typography>
+//           <div className="flex items-center gap-4">
+//             <div className="mr-4 hidden lg:block">{navList}</div>
+//             <Button
+//               type="button"
+//               color="blue-gray"
+//               size="sm"
+//               className="hidden lg:inline-block"
+//             >
+//               <span>Buy Now</span>
+//             </Button>
+//             <IconButton
+//               variant="text"
+//               className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+//               ripple={false}
+//               onClick={() => setOpenNav(!openNav)}
+//             >
+//               {openNav ? (
+//                 <svg
+//                   xmlns="http://www.w3.org/2000/svg"
+//                   fill="none"
+//                   className="h-6 w-6"
+//                   viewBox="0 0 24 24"
+//                   stroke="currentColor"
+//                   strokeWidth={2}
+//                 >
+//                   <path
+//                     strokeLinecap="round"
+//                     strokeLinejoin="round"
+//                     d="M6 18L18 6M6 6l12 12"
+//                   />
+//                 </svg>
+//               ) : (
+//                 <svg
+//                   xmlns="http://www.w3.org/2000/svg"
+//                   className="h-6 w-6"
+//                   fill="none"
+//                   stroke="currentColor"
+//                   strokeWidth={2}
+//                 >
+//                   <path
+//                     strokeLinecap="round"
+//                     strokeLinejoin="round"
+//                     d="M4 6h16M4 12h16M4 18h16"
+//                   />
+//                 </svg>
+//               )}
+//             </IconButton>
+//           </div>
+//         </div>
+//         <MobileNav open={openNav}>
+//           {navList}
+//           <Button size="sm" fullWidth className="mb-2 bg-teal-800">
+//             <span>Buy Now</span>
+//           </Button>
+//         </MobileNav>
+//       </Navbar>
+//       <div className="mx-auto max-w-screen-md py-12">
+//         <Card className="mb-12 overflow-hidden rounded-xl">
+//           <img
+//             alt="nature"
+//             className="h-[32rem] w-full object-cover object-center"
+//             src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
+//           />
+//         </Card>
+//         <Typography variant="h2" color="blue-gray" className="mb-2">
+//           Dear Jeju, “Thank You!” 
+//         </Typography>
+//         <Typography color="gray" className="font-normal">
+//           Welcome to our community service website! We are a group of individuals dedicated 
+//           to making a positive impact in our community through various service projects and events. 
+//           Whether you are an individual looking to get involved and give back, or an organization 
+//           seeking volunteers for your cause, we hope to connect you with opportunities to make a 
+//           difference. We collaborate with BHA and SJA to help protect the environment on Jeju Island. 
+//           Together, we address environmental issues such as pollution by combining resources. 
+//           Whether through cleanups, tree planting, or educational campaigns, we work to raise 
+//           awareness and drive sustainable change in the community. Our community service group 
+//           stands out by collaborating with other organizations, prioritizing education and 
+//           awareness, and using an online store to raise funds. We are a group of individuals 
+//           dedicated to making a positive impact in our community through various service projects 
+//           and events. 
+//         </Typography>
+//       </div>
+//     </>
+//   );
+// }
