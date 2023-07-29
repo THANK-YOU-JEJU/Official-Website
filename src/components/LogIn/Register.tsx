763784592ef891039d00eb59724ad6789e5be544
SadPig0 <119029502+SadPig0@users.noreply.github.com>
2023-07-29 09:47:48 +0900

    again

--- src/components/LogIn/Register.tsx
+++ src/components/LogIn/Register.tsx
@@ -10,7 +10,7 @@ const Register = () => {
         <label className="block mb-1" >Email-Address</label>
         <input id="email" type="text" name="email" className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
       </div>
-      <div className="mb-4">
+      <div className="mb-4"> 
         <label className="block mb-1" >Password</label>
         <input id="password" type="password" name="password" className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
       </div>
@@ -25,7 +25,7 @@ const Register = () => {
       </div>
     </form>
   </div>
-</div>
+    </div>
   </div>
   )
 }
