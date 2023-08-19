c87b17c2bf6a54fb720b1acf0f3422a29cf9043f
Doyeon Kim <s21250535@sjajeju.kr>
2023-08-19 16:01:10 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import routes from "./route";
 import Home from "./components/Home/Home";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Gallery from "./components/Gallery/Gallery";
